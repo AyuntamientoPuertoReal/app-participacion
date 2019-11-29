e13c8040d0105482848d47ffd1f74e69f18e8b6c
jmmateoor <josem.mateo.ortega@gmail.com>
2019-11-29 13:11:47 +0100

    Creación de widget para mostrar sin conexión

--- lib/src/widgets/widget_no_connection.dart
+++ lib/src/widgets/widget_no_connection.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
 
-Widget widgetNoConnection(String mensaje){
+Widget noConnectionToInternet(){
   return Container(
-    padding: EdgeInsets.all(40),
+    padding: EdgeInsets.all(20),
     child:Column(
       children: <Widget>[
-        Center(child:Text(mensaje)),
-        Image(
-          image: AssetImage("assets/img/jar-loading.gif"),
-        )
+        Center(child: Text('INFORMACIÓN',style: TextStyle(fontSize: 22, fontWeight: FontWeight.w800, color: Color.fromRGBO(162, 0, 125, 1.0)),),),
+        Center(
+          child: Image(
+            image: AssetImage("assets/img/conexion-internet.png"),
+          ),
+        ),
+        Center(child:Text('El dispositivo no tiene conexión a Internet.', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600))),
+      ],
+    )
+  );
+}
+
+Widget noConnectionToServer(){
+  return Container(
+    padding: EdgeInsets.all(20),
+    child:Column(
+      children: <Widget>[
+        Center(child: Text('INFORMACIÓN',style: TextStyle(fontSize: 22, fontWeight: FontWeight.w800, color: Color.fromRGBO(162, 0, 125, 1.0)),),),
+        Center(
+          child: Image(
+            image: AssetImage("assets/img/conexion-server.png"),
+          ),
+        ),
+        Center(child:Text('El servicio está bajo mantenimiento. Vuelva a intentarlo de nuevo más tarde.', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600))),
+        SizedBox(height: 20,),
+        Center(child: Text('Disculpe las molestias',style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600,color: Color.fromRGBO(162, 0, 125, 1.0))),)
       ],
     )
   );
